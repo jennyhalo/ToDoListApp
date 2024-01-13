111e581a2ddc2badca9a61b5d87de94a9f62ad99
Jenny-Carolina Halonen <91306830+jennyhalo@users.noreply.github.com>
2024-01-13 03:46:26 +0200

    authentication validation stuff

--- ToDoListCreatorApp/src/app/register/register.page.ts
+++ ToDoListCreatorApp/src/app/register/register.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthenticationService } from '../authentication.service';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-
-  constructor() { }
+  regForm: FormGroup | undefined;
+  constructor(public formBuilder:FormBuilder, public loadingCtrl:LoadingController, public authService:AuthenticationService) {}
 
   ngOnInit() {
+    this.regForm = this.formBuilder.group({
+      username: ['', [Validators.required]],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email,
+          Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$'),
+        ],
+      ],
+      password: [
+        '',
+        Validators.required,
+        Validators.pattern('(?=.*d)(?=.*[a-z])(?=.*[A-Z]).{8,}'),
+      ],
+    });
+  }
+  get errorControl(){
+    return this.regForm?.controls;
   }
 
+  async register(){
+    const loading = await this.loadingCtrl.create();
+    await loading.present();
+    if(this.regForm?.valid){
+      // const user = await this.authService.registerUser(email, password)
+    }
+  }
 }
